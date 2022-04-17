b9100384c4156ab1865f3b5ccc371447b4cd9fbd
Anak Agung Bintang <85743876+gekbbb@users.noreply.github.com>
2022-04-17 21:16:56 +0700

    Add files via upload

---
+++ Symmetric Ciphers - Block Ciphers & Stream Ciphers/bean counter.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import binascii
+from PIL import Image
+
+url = 'http://aes.cryptohack.org/bean_counter/encrypt/'
+
+def xor_hex(a, b):
+    temp = int(a, 16) ^ int(b, 16)
+    res = hex(temp)[2:].zfill(2)
+    return res
+
+def encrypted():
+    req = requests.get(url)
+    enc = req.json()['encrypted']
+    return enc
+
+def decrypted(encm, key):
+    dec = ''
+    t = 0
+    for i in range(0, len(encm), 2):
+        temp =xor_hex(encm[i:i+2], key[t:t+2])
+        t += 2
+        if (t >= len(key)):
+            t = 0
+        dec += temp
+    d = bytes.fromhex(dec)
+    with open('beancounter.png', 'wb') as f:
+        f.write(d)
+
+enc = encrypted()
+encpic = binascii.unhexlify(enc)
+pngheader = '89504e470d0a1a0a0000000d49484452'
+
+print(len(enc), len(enc) % 32)
+
+key1 = enc[:32]
+key = ''
+for i in range(0, len(key1), 2):
+	temp = xor_hex(key1[i:i+2], pngheader[i:i+2])
+	key += temp
+
+decrypted(enc, key)
+im = Image.open(r"beancounter.png")
+im.show()
